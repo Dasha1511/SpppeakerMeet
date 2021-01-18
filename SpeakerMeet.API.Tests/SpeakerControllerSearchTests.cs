d22574c9d2a60e5d5368d78c1f7a8e8f18614d92
Dasha1511 <70947760+Dasha1511@users.noreply.github.com>
2021-01-18 14:55:31 +0300

    Update SpeakerControllerSearchTests.cs

--- SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -40,5 +40,16 @@ public void ItHasSearch() //Проверяем наличие метода По�
             Assert.NotNull(result.Value);
             Assert.IsType<List<Speaker>>(result.Value);
         }
+        [Theory]
+        [InlineData("Joshua")]
+        [InlineData("joshua")]
+        [InlineData("JoShUa")]
+        public void GivenCaseInsensitveMatchThenSpeakerInCollection (string searchString)
+        {
+        var result = _controller.Search(searchString) as OkObjectResult;
+        var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+        Assert.Equal(1, speakers.Count);
+        Assert.Equal("Joshua", speakers[0].Name);
+        } 
     }
 }
