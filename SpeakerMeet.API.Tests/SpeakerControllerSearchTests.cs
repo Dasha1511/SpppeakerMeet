e3489a4a5eba4f04b9b70e0c26db833d8c144718
Dasha1511 <70947760+Dasha1511@users.noreply.github.com>
2021-01-20 14:39:49 +0300

    Update SpeakerControllerSearchTests.cs

--- SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -3,32 +3,34 @@ using Xunit;
 using System.Collections.Generic;
 using SpeakerMeet.API.Controllers; //<— directive to add
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 
 
 namespace SpeakerMeet.API.Tests
 {
-public class SpeakerControllerSearchTests //<— to rename
+    public class SpeakerControllerSearchTests //<— to rename
     {
-    [Fact]
-    public void ItExists()
+        [Fact]
+        public void ItExists()
         {
-        var controller = new SpeakerController();
+            var controller = new SpeakerController();
         }
 
-[Fact]
-public void ItHasSearch() //Проверяем наличие метода Поиск
-    {
-    var controller = new SpeakerController();
-    controller.Search("Jos");
-    }
-
-[Fact]
-    public void ItReturnsOkObjectResult()
+        [Fact]
+        public void ItHasSearch() //Проверяем наличие метода Поиск
         {
-        var controller = new SpeakerController();
-        var result = controller.Search("Jos");
-        Assert.NotNull(result); //Поиск принес результат?
-        Assert.IsType<OkObjectResult>(result); //Результат ОК?
+            var controller = new SpeakerController();
+            controller.Search("Jos");
+        }
+
+        [Fact]
+        public void ItReturnsOkObjectResult()
+        {
+            var controller = new SpeakerController();
+            var result = controller.Search("Jos");
+            Assert.NotNull(result); //Поиск принес результат?
+            Assert.IsType<OkObjectResult>(result); //Результат ОК?
         }
        
         [Fact]
@@ -40,16 +42,51 @@ public void ItHasSearch() //Проверяем наличие метода По�
             Assert.NotNull(result.Value);
             Assert.IsType<List<Speaker>>(result.Value);
         }
+
+        [Fact/*(Skip = "Более не требуется")*/]
+        public void GivenExactMatchThenOneSpeakerInCollection()
+        {
+            var result = _controller.Search("Joshua") as OkObjectResult;
+            var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+            Assert.Equal(1, speakers.Count);
+        }
+
         [Theory]
         [InlineData("Joshua")]
         [InlineData("joshua")]
         [InlineData("JoShUa")]
         public void GivenCaseInsensitveMatchThenSpeakerInCollection (string searchString)
         {
-        var result = _controller.Search(searchString) as OkObjectResult;
-        var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
-        Assert.Equal(1, speakers.Count);
-        Assert.Equal("Joshua", speakers[0].Name);
+            var result = _controller.Search(searchString) as OkObjectResult;
+            var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+            Assert.Equal(1, speakers.Count);
+            Assert.Equal("Joshua", speakers[0].Name);
+        }
+
+        [Fact]
+        public void GivenNoMatchThenEmptyCollection()
+        {
+            var result = _controller.Search("ZZZ") as OkObjectResult;
+            var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+            Assert.Equal(0, speakers.Count);
+        }
+
+        [Fact]
+        public void Given3MatchThenCollectionWith3Speakers()
+        {
+            var result = _controller.Search("jos") as OkObjectResult;  
+            var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+            Assert.Equal(3, speakers.Count);
+            Assert.True(speakers.Any(s => s.Name == "Josh"));
+            Assert.True(speakers.Any(s => s.Name == "Joshua"));
+            Assert.True(speakers.Any(s => s.Name == "Joseph"));
         } 
+
+        private readonly SpeakerController _controller;
+        public SpeakerControllerSearchTests()
+        {
+            _controller = new SpeakerController();
+        }
+        
     }
 }
